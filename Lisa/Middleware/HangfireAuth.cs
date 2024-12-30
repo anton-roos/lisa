97d5080268d58f999f9a51283f1b3f554a06d7ef
Anton Roos <antonroos992@gmail.com>
2024-12-30 22:30:08 +0200

    fix error

--- Lisa/Middleware/HangfireAuth.cs
+++ Lisa/Middleware/HangfireAuth.cs
@@ -7,7 +7,6 @@ public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
     public bool Authorize(DashboardContext context)
     {
         var httpContext = context.GetHttpContext();
-        // Allow access only if the user is authenticated and in the "Admin" role
-        return httpContext.User.Identity.IsAuthenticated && httpContext.User.IsInRole(Roles.SystemAdministrator);
+        return httpContext.User?.Identity?.IsAuthenticated == true && httpContext.User.IsInRole(Roles.SystemAdministrator);
     }
 }

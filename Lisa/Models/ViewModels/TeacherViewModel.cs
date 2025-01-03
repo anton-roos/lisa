3c1ca858437479bbe45d7e9d46af46554d47a430
Anton Roos <antonroos992@gmail.com>
2025-01-03 21:24:38 +0200

    minor UI consistencies

--- Lisa/Models/ViewModels/TeacherViewModel.cs
+++ Lisa/Models/ViewModels/TeacherViewModel.cs
@@ -4,6 +4,10 @@ namespace Lisa.Models.ViewModels;
 
 public class TeacherViewModel
 {
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
     public string? Email { get; set; }
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+    public string? Password { get; set; }
 }

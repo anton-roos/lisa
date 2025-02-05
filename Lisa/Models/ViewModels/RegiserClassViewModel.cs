b5609fc87b182cb0ff8d6d92a6ec3799dca16739
Anton Roos <antonr@hollard.co.za>
2025-02-05 11:23:58 +0200

    register class

--- Lisa/Models/ViewModels/RegiserClassViewModel.cs
+++ Lisa/Models/ViewModels/RegiserClassViewModel.cs
@@ -6,6 +6,7 @@ public class RegisterClassViewModel
 {
     public string? Name { get; set; }
     public Guid GradeId { get; set; }
+    public SchoolGrade? Grade { get; set; }
     public Guid TeacherId { get; set; }
     public List<int> SubjectIds { get; set; } = [];
 }

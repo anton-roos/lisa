cd2d21f745122b8048cdfa5a723575777584ec39
anton-roos <antonroos992@gmail.com>
2025-10-07 18:49:23 +0200

    fix logout issues

--- src/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -7,30 +7,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Lisa.Areas.Identity.Pages.Account;
 
-public class LogoutModel : PageModel
+public class LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger) : PageModel
 {
-    private readonly SignInManager<User> _signInManager;
-    private readonly ILogger<LogoutModel> _logger;
-
-    public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+    public async Task<IActionResult> OnGet(string returnUrl = null)
     {
-        _signInManager = signInManager;
-        _logger = logger;
+        await signInManager.SignOutAsync();
+        logger.LogInformation("User logged out.");
+        
+        if (!string.IsNullOrEmpty(returnUrl))
+        {
+            return LocalRedirect(returnUrl);
+        }
+        
+        return LocalRedirect("~/");
     }
 
     public async Task<IActionResult> OnPost(string returnUrl = null)
     {
-        await _signInManager.SignOutAsync();
-        _logger.LogInformation("User logged out.");
-        if (returnUrl != null)
+        await signInManager.SignOutAsync();
+        logger.LogInformation("User logged out.");
+        
+        if (!string.IsNullOrEmpty(returnUrl))
         {
             return LocalRedirect(returnUrl);
         }
-        else
-        {
-            // This needs to be a redirect so that the browser performs a new
-            // request and the identity for the user gets updated.
-            return RedirectToPage();
-        }
+
+        return LocalRedirect("~/");
     }
 }
